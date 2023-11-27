a030a9ad8cf9b0c3637830baa3aa96db05d95354
my-name-is-gabs <villar.gabriel.p.6@gmail.com>
2023-11-27 14:01:02 +0800

    revert

--- src/pages/Faqs/FacebookCustomChat.jsx
+++ src/pages/Faqs/FacebookCustomChat.jsx
@@ -4,7 +4,7 @@ import { FacebookProvider, CustomChat } from "react-facebook";
 const FacebookCustomChat = () => {
   return (
     <FacebookProvider appId="716748153348825" chatSupport>
-      <CustomChat pageId="192715333915300" minimized={true} />
+      <CustomChat pageId="192715333915300" minimized={false} />
     </FacebookProvider>
   );
 };
