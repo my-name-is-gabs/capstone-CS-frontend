d410ed62d7e06a66246296d58c04411a0c37218d
my-name-is-gabs <villar.gabriel.p.6@gmail.com>
2023-11-28 09:54:34 +0800

    trial2

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-import { BASE_URL, API_TIMEOUT } from "../constant";
+import { BASE_URL } from "../constant";
 
 export default axios.create({
   baseURL: BASE_URL,
-  timeout: API_TIMEOUT,
+  // timeout: API_TIMEOUT,
   headers: {
     Authorization: localStorage.getItem("access_token")
       ? "JWT " + localStorage.getItem("access_token")
