ab53643524c2bd41a6ca364c0579f638bc179414
my-name-is-gabs <villar.gabriel.p.6@gmail.com>
2023-11-28 09:53:40 +0800

    trial 2

--- src/constant/index.js
+++ src/constant/index.js
@@ -5,4 +5,4 @@ export const INITIAL_FORM_COUNT = 1;
 // AXIOS API CONSTANTS
 // export const BASE_URL = "http://127.0.0.1:8000";
 export const BASE_URL = "https://centrosecretariat-docker.onrender.com";
-export const API_TIMEOUT = 5000;
+export const API_TIMEOUT = 120000;
