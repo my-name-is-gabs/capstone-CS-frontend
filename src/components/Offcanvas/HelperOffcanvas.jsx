0d55c71c89fef1af79f058d9c1b4c4d36f5736e7
my-name-is-gabs <villar.gabriel.p.6@gmail.com>
2024-01-13 18:52:41 +0800

    nakalimutan

---
+++ src/components/Offcanvas/HelperOffcanvas.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+
+const HelperOffcanvas = ({ title, element }) => {
+  return (
+    <div
+      className="offcanvas offcanvas-end"
+      tabIndex="-1"
+      id="offcanvasRight"
+      aria-labelledby="offcanvasRightLabel"
+    >
+      <div className="offcanvas-header">
+        <h5 className="offcanvas-title" id="offcanvasRightLabel">
+          {title}
+        </h5>
+        <button
+          type="button"
+          className="btn-close"
+          data-bs-dismiss="offcanvas"
+          aria-label="Close"
+        ></button>
+      </div>
+      <div className="offcanvas-body">{element}</div>
+    </div>
+  );
+};
+
+export default HelperOffcanvas;
